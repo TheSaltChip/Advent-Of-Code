e8d0ed240b4c00db6fb60d77b371969151ea90f8
Sebastian Misje Jonassen <smj@dataloy.com>
2024-12-05 15:14:31 +0100

    Day 3 part 2 (dont reset do after each line:))

--- 2024/day3/day3.go
+++ 2024/day3/day3.go
@@ -1,11 +1,14 @@
-﻿package day3
+package day3
 
 import (
 	"2024/util"
 	"bufio"
+	"github.com/adam-lavrik/go-imath/ix"
 	"log"
 	"os"
 	"regexp"
+	"sort"
+	"strings"
 )
 
 func Part1() {
@@ -103,11 +106,12 @@ func Part2() {
 	scanner := bufio.NewScanner(file)
 
 	sum := 0
+	do := true
 
 	for scanner.Scan() {
-		line := util.StripBOM(scanner.Text())
+		line := scanner.Text()
 
-		regexMul, err := regexp.Compile(`mul\(\d+,\d+\)`)
+		regexMul, err := regexp.Compile(`mul\(\d{1,3},\d{1,3}\)`)
 		regexDo, err := regexp.Compile(`do\(\)`)
 		regexDont, err := regexp.Compile(`don't\(\)`)
 
@@ -119,11 +123,50 @@ func Part2() {
 		doIndecies := regexDo.FindAllStringIndex(line, -1)
 		dontIndecies := regexDont.FindAllStringIndex(line, -1)
 
-		doI, dontI := 0, 0
+		combinedMap := make(map[int][]string)
 
-		do := true
+		maxI := ix.Maxs(len(resultIndecies), len(doIndecies), len(dontIndecies))
 
-		for _, res := range resultIndecies {
+		for i := 0; i < maxI; i++ {
+			if i < len(resultIndecies) {
+				res := resultIndecies[i]
+				combinedMap[res[0]] = append(combinedMap[res[0]], line[res[0]:res[1]])
+			}
+			if i < len(doIndecies) {
+				res := doIndecies[i]
+				combinedMap[res[0]] = append(combinedMap[res[0]], line[res[0]:res[1]])
+			}
+			if i < len(dontIndecies) {
+				res := dontIndecies[i]
+				combinedMap[res[0]] = append(combinedMap[res[0]], line[res[0]:res[1]])
+			}
+		}
+
+		keys := make([]int, 0, len(combinedMap))
+		for k := range combinedMap {
+			keys = append(keys, k)
+		}
+
+		sort.Ints(keys)
+
+		for _, key := range keys {
+			if do && regexMul.MatchString(combinedMap[key][0]) {
+				numStrs := line[key+4 : key+4+strings.Index(line[key+4:], ")")]
+				nums, err := util.ToIntArray(numStrs, ",")
+
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				sum += nums[0] * nums[1]
+			} else if regexDo.MatchString(combinedMap[key][0]) {
+				do = true
+			} else if regexDont.MatchString(combinedMap[key][0]) {
+				do = false
+			}
+		}
+
+		/*for _, res := range resultIndecies {
 
 			for _, dontIndecy := range dontIndecies {
 				if res[0] > dontIndecy[0] {
@@ -152,7 +195,7 @@ func Part2() {
 			}
 
 			sum += nums[0] * nums[1]
-		}
+		}*/
 	}
 
 	log.Println(sum)
